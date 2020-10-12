88ac4622deb9dbe32884f99cde18dd2a564c365d
Yahia <yahia.mostafa.elsayed@gmail.com>
2020-10-12 18:57:16 +0200

    Update widgets

--- app/src/main/java/com/noted/noted/receiver/NotesWidgetProvider.kt
+++ app/src/main/java/com/noted/noted/receiver/NotesWidgetProvider.kt
@@ -5,8 +5,11 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
 import android.widget.RemoteViews
 import com.noted.noted.R
+import com.noted.noted.service.NotesWidgetService
 import com.noted.noted.view.activity.NotesWidgetConfigureActivity
 
 
@@ -20,24 +23,42 @@ class NotesWidgetProvider : AppWidgetProvider() {
         // Perform this loop procedure for each App Widget that belongs to this provider
         appWidgetIds.forEach { appWidgetId ->
             // Create an Intent to launch ExampleActivity
-            val pendingIntent: PendingIntent = Intent(context, NotesWidgetConfigureActivity::class.java)
-                .let { intent ->
-                    PendingIntent.getActivity(context, 0, intent, 0)
-                }
+            val intent = Intent(context, NotesWidgetService::class.java).apply {
+                // Add the app widget ID to the intent extras.
+                putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+                data = Uri.parse(toUri(Intent.URI_INTENT_SCHEME))
+            }
+            // Instantiate the RemoteViews object for the app widget layout.
+            val rv = RemoteViews(context.packageName, R.layout.notes_widget_layout).apply {
+                // Set up the RemoteViews object to use a RemoteViews adapter.
+                // This adapter connects
+                // to a RemoteViewsService  through the specified intent.
+                // This is how you populate the data.
+                setRemoteAdapter(R.id.notes_widget_grid, intent)
 
-            // Get the layout for the App Widget and attach an on-click listener
-            // to the button
-            val views: RemoteViews = RemoteViews(
-                context.packageName,
-                R.layout.notes_widget_layout
-            ).apply {
-                setOnClickPendingIntent(R.id.button, pendingIntent)
+                // The empty view is displayed when the collection has no items.
+                // It should be in the same layout used to instantiate the RemoteViews
+                // object above.
+                setEmptyView(R.id.notes_widget_grid, R.id.empty_view)
             }
 
+            //
+            // Do additional processing specific to this app widget...
+            //
+
             // Tell the AppWidgetManager to perform an update on the current app widget
-            appWidgetManager.updateAppWidget(appWidgetId, views)
+            appWidgetManager.updateAppWidget(appWidgetId, rv)
         }
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+
     }
 
-
+    override fun onAppWidgetOptionsChanged(
+        context: Context?,
+        appWidgetManager: AppWidgetManager?,
+        appWidgetId: Int,
+        newOptions: Bundle?
+    ) {
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions)
+    }
 }
---
+++ app/src/main/java/com/noted/noted/service/NotesWidgetService.kt
@@ -0,0 +1,93 @@
+package com.noted.noted.service
+
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+import android.widget.RemoteViewsService
+import androidx.core.content.res.ResourcesCompat
+import com.noted.noted.R
+import com.noted.noted.model.Note
+import io.noties.markwon.AbstractMarkwonPlugin
+import io.noties.markwon.Markwon
+import io.noties.markwon.MarkwonVisitor
+import io.noties.markwon.ext.strikethrough.StrikethroughPlugin
+import io.noties.markwon.ext.tasklist.TaskListPlugin
+import io.realm.Realm
+import org.commonmark.node.SoftLineBreak
+
+class NotesWidgetService : RemoteViewsService() {
+    override fun onGetViewFactory(intent: Intent): RemoteViewsFactory {
+
+        return GridViewFactory(this.applicationContext, intent)
+    }
+
+
+}
+
+class GridViewFactory(
+    private val context: Context,
+    intent: Intent
+) : RemoteViewsService.RemoteViewsFactory {
+    private lateinit var notesList:List<Note>
+    private lateinit var  markwon: Markwon
+
+    override fun onCreate() {
+        val realm = Realm.getDefaultInstance()
+        val realmResults = realm.where(Note::class.java).findAll()
+        notesList = realm.copyFromRealm(realmResults)
+
+
+        markwon = Markwon.builder(context)
+            .usePlugin(StrikethroughPlugin.create())
+            .usePlugin(TaskListPlugin.create(ResourcesCompat.getColor(context.resources, R.color.primary, context.theme), ResourcesCompat.getColor(context.resources, R.color.primary, context.theme), ResourcesCompat.getColor(context.resources, R.color.background, context.theme)))
+            .usePlugin(object : AbstractMarkwonPlugin() {
+                override fun configureVisitor(builder: MarkwonVisitor.Builder) {
+                    super.configureVisitor(builder)
+                    builder.on(
+                        SoftLineBreak::class.java
+                    ) { visitor, _ -> visitor.forceNewLine() }
+                }
+            })
+            .build()
+    }
+
+    override fun onDestroy() {
+
+    }
+
+    override fun getCount(): Int {
+       return notesList.size
+    }
+
+    override fun getViewAt(position: Int): RemoteViews {
+        val note = notesList[position]
+        val body = markwon.toMarkdown(note.body)
+
+        return RemoteViews(context.packageName, R.layout.notes_widget_item).apply {
+            setTextViewText(R.id.widget_note_title, note.title)
+            setTextViewText(R.id.widget_note_body, body)
+
+        }
+    }
+
+    override fun getLoadingView(): RemoteViews {
+        return RemoteViews(context.packageName, R.layout.notes_widget_item)
+    }
+
+    override fun getViewTypeCount(): Int {
+        return 1
+    }
+
+    override fun hasStableIds(): Boolean {
+        return true
+    }
+
+    override fun onDataSetChanged() {
+
+    }
+
+    override fun getItemId(position: Int): Long {
+        return notesList[position].id
+    }
+}
+
